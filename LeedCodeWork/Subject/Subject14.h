4ade70527115c8565c6caa8c77e1c20a25226b7d
boovit <boovit@163.com>
2021-11-19 11:16:09 +0800

    add files 14题

---
+++ LeedCodeWork/Subject/Subject14.h
@@ -0,0 +1,16 @@
+//
+//  Subject14.h
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/18.
+//
+
+#import "SubjectObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Subject14 : SubjectObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeedCodeWork/Subject/Subject14.m
@@ -0,0 +1,41 @@
+//
+//  Subject14.m
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/18.
+//
+
+#import "Subject14.h"
+
+/**
+ 最长公共长度
+ ["flower","flab","flibii"]
+ "fl"
+ */
+
+//循环比较每一位，直到出现不同时，结束比较。
+@implementation Subject14
+-(NSString*)longestCommonPrefix:(NSArray<NSString*>*)strs
+{
+    NSString *restr=@"";
+    BOOL flag = YES;
+    NSInteger index = -1;
+    while (flag) {
+        index++;
+        NSString *compareStr = nil;
+        for (NSString *itemStr in strs) {
+            if (compareStr==nil) {
+                compareStr = [itemStr substringWithRange:NSMakeRange(index, 1)];
+            }else{
+                NSString *temp = [itemStr substringWithRange:NSMakeRange(index, 1)];
+                if (![compareStr isEqualToString:temp]) {//出现不同字符，结束while循环
+                    flag = NO;
+                    break;
+                }
+            }
+        }
+    }
+    restr = [strs[0] substringWithRange:NSMakeRange(0,index+1)];
+    return restr;
+}
+@end
