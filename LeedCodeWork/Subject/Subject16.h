bb70c09e5c9cab213d5e98db200598ac86e845d8
boovit <boovit@163.com>
2021-11-19 17:01:53 +0800

    add files 16

---
+++ LeedCodeWork/Subject/Subject16.h
@@ -0,0 +1,16 @@
+//
+//  Subject16.h
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/19.
+//
+
+#import "SubjectObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Subject16 : SubjectObject
+
+@end
+
+NS_ASSUME_NONNULL_END
