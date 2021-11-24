833097382a5401653e13d8d8983aac675a689fb7
boovit <boovit@163.com>
2021-11-24 14:07:11 +0800

    finish 19

--- LeedCodeWork/Subject/Subject19.m
+++ LeedCodeWork/Subject/Subject19.m
@@ -8,6 +8,9 @@
 #import "Subject19.h"
 /**
  给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ 
+ 1->2->3->4->5  n=2
+ 1->2->3->5
  */
 
 @interface ListNode : NSObject
@@ -19,9 +22,22 @@
 @end
 
 @implementation Subject19
+//双指针测距法
 -(ListNode*)removeNthFromEnd:(ListNode*)head n:(int)n
 {
-    ListNode *resNode = head;
-    return resNode;
+    ListNode *preNode = [ListNode new];//前向节点
+    preNode.next = head;
+    ListNode *fir = head;
+    ListNode *sec = head;
+    for (int i=0; i<n; i++) {
+        fir = fir.next;
+    }
+    while (fir.next!=nil) {
+        fir = fir.next;
+        sec = sec.next;
+    }
+    
+    sec.next = sec.next.next;
+    return preNode.next;
 }
 @end
