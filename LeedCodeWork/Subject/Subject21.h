365255b56882d7461180dcb8ce2dbc0d761e953c
boovit <boovit@163.com>
2021-11-24 15:50:44 +0800

    add files 21
    解决冲突
    
    解决冲突
    
    rebase 解决冲突

---
+++ LeedCodeWork/Subject/Subject21.h
@@ -0,0 +1,16 @@
+//
+//  Subject21.h
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/24.
+//
+
+#import "SubjectObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Subject21 : SubjectObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LeedCodeWork/Subject/Subject21.m
@@ -0,0 +1,21 @@
+//
+//  Subject21.m
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/24.
+//
+
+#import "Subject21.h"
+
+/**
+ 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ 
+ */
+
+@implementation Subject21
+-(ListNode*)mergeTwoLists:(ListNode*)firstNode second:(ListNode*)secNode
+{
+    ListNode *resNode = nil;
+    return ListNode;
+}
+@end
