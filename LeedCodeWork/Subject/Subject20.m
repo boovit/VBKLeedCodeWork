bab90ebb736796cb7701c55dbe41c74a45c8993d
boovit <boovit@163.com>
2021-11-24 14:31:17 +0800

    add files 20

---
+++ LeedCodeWork/Subject/Subject20.m
@@ -0,0 +1,38 @@
+//
+//  Subject20.m
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/24.
+//
+
+#import "Subject20.h"
+
+/**
+ 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+
+ 有效字符串需满足：
+ 左括号必须用相同类型的右括号闭合。
+ 左括号必须以正确的顺序闭合。
+ 
+ 输入：s = "( ) [ ] { }"
+ 输出：true
+ 
+ 输入：s = "( [ ) ]"
+ 输出：false
+ 
+ 输入：s = "{ [ ] }"
+ 输出：true
+
+ 链接：https://leetcode-cn.com/problems/valid-parentheses
+ */
+
+@implementation Subject20
+//使用栈结构
+-(BOOL)isValid:(NSString*)string
+{
+    BOOL res = NO;
+    return res;
+}
+
+-()
+@end
