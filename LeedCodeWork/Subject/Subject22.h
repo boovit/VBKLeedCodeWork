010ebb207178918a22b815fbd4b3f1980b5f25c4
boovit <boovit@163.com>
2021-12-14 17:02:37 +0800

    finish 22

---
+++ LeedCodeWork/Subject/Subject22.h
@@ -0,0 +1,16 @@
+//
+//  Subject22.h
+//  LeedCodeWork
+//
+//  Created by weibo on 2021/11/24.
+//
+
+#import "SubjectObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Subject22 : SubjectObject
+
+@end
+
+NS_ASSUME_NONNULL_END
